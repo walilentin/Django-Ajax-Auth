4a035f0f788e93e952965203609abe2abd1bfeb9
walilentin <grebenukvalentin@icloud.com>
2023-05-12 14:13:12 +0300

    add login

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html'),name='home'),
+    path('admin', admin.site.urls),
+    path('user', include('user.urls')),
 ]
