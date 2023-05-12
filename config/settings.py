d565bec21461193db562a2808c19753ff45cb235
walilentin <grebenukvalentin@icloud.com>
2023-05-12 13:32:48 +0300

    tree commit

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'user.User'
 # Application definition
 
 INSTALLED_APPS = [
@@ -26,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # --- My apps --- #
-    'shop',
+    'user',
     'authentication',
 ]
 
